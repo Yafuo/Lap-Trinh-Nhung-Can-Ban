51982d194461c656787d4ff8a833b4a9d26b3661
Yafuo <44750263+Yafuo@users.noreply.github.com>
2018-12-18 08:17:22 +0700

    Update handler-login.js

--- main/webapp/js/handler-login.js
+++ main/webapp/js/handler-login.js
@@ -1,6 +1,6 @@
 document.onkeydown= function(event) {
 	event= event || window.event;
-	if(event.keyCode === 6 || event.keyCode === 7 || event.keyCode === 65) {
+	if(event.keyCode === 6 || event.keyCode === 7 || event.keyCode === 32) {
 		alert("Board is connected");
 	}
 }
