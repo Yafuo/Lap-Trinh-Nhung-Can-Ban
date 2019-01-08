9c613d2682e402ec481431c77f6de9504294dc4f
Yafuo <44750263+Yafuo@users.noreply.github.com>
2019-01-08 08:17:05 +0700

    Add files via upload

---
+++ main/java/com/gpu/hello/Position.java
@@ -0,0 +1,20 @@
+package com.gpu.hello;
+
+public class Position {
+
+	private int pos;
+
+	public Position() {}
+	
+	public Position(int pos) {
+		this.pos = pos;
+	}
+
+	public int getPos() {
+		return pos;
+	}
+
+	public void setPos(int pos) {
+		this.pos = pos;
+	}
+}
